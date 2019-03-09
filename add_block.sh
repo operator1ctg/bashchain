10798fcc264fa5e6aad52d69e4d863aa87f67571
operator1ctg <ops1@myself.com>
2019-03-09 22:20:35 +0000

    Update add_block.sh

--- add_block.sh
+++ add_block.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-payload=`echo "${1}"|head -n1|tr -d ':'`
-previous_block=`tail -n1 'database'`
-id=$((${previous_block%%:*}+1))
+payload=`echo "${1}"|head -n1|tr -d ':'`; #generate the active payload
+previous_block=`tail -n1 'database'`; #grab the last known block from the 'database' record
+id=$((${previous_block%%:*}+1)); # increment
 while : ; do
-	nonce=$RANDOM
+	nonce=$RANDOM; # generate a quasi-random seed to test against
 	checksum=`echo "${previous_block##*:}:$id:$payload:$nonce"|sha256sum|sed 's/[ -]*$//'`;
-	ndr=$(($nonce - $RANDOM));
-	ndr="${ndr: -2}";
+	ndr=$(($nonce - $RANDOM)); # generate a quasi-number ( based on the original nonce seed )
+	ndr="${ndr: -2}"; # grab the last 2 characters from the seed.
 	#[[ "${checksum: -2}" == '00' ]] && break;
-	[[ "${checksum: -2}" == "${ndr}" ]] && break;
-done
-echo "$id:$payload:$nonce:$checksum">>'database';
+	[[ "${checksum: -2}" == "${ndr}" ]] && break; # test the cs-hash against the ndr
+done;
+echo "$id:$payload:$nonce:$checksum">>'database'; #feedback the results to terminal
